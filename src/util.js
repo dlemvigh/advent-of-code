5568e4c757a2e2b89aa24412b03956c00415973b
David Lemvigh <dle@siteimprove.com>
2019-12-09 16:22:48 +0100

    tweaks to util

--- src/util.js
+++ src/util.js
@@ -1,5 +1,8 @@
 const fs = require("fs");
 const path = require("path");
 
-exports.readInput = (folder, filename = "input.txt") =>
-  fs.readFileSync(path.join(folder, filename)).toString();
+function readInput(folder, filename = "input.txt") {
+  return fs.readFileSync(path.join(folder, filename)).toString();
+}
+
+module.exports = { readInput };
