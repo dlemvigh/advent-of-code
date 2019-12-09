1ff1173930f15c2c04bafa01d31e1ecd8b4894ab
David Lemvigh <dle@siteimprove.com>
2019-12-09 16:44:12 +0100

    opcode async rewrite

--- src/2019/day7/code.js
+++ src/2019/day7/code.js
@@ -1,48 +1,37 @@
+const { permutations, Queue } = require("../../util");
+
 const input =
   "3,8,1001,8,10,8,105,1,0,0,21,42,67,84,97,118,199,280,361,442,99999,3,9,101,4,9,9,102,5,9,9,101,2,9,9,1002,9,2,9,4,9,99,3,9,101,5,9,9,102,5,9,9,1001,9,5,9,102,3,9,9,1001,9,2,9,4,9,99,3,9,1001,9,5,9,1002,9,2,9,1001,9,5,9,4,9,99,3,9,1001,9,5,9,1002,9,3,9,4,9,99,3,9,102,4,9,9,101,4,9,9,102,2,9,9,101,3,9,9,4,9,99,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,99,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,99,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,2,9,4,9,99,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,99,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,99";
 const program = input.split(",").map(Number);
 
 // amp([1, 0, 4, 3, 2], 0);
-const perm = permutations([0, 1, 2, 3, 4]).map(perm => perm.map(Number));
+findBest();
 
-(function findBest() {
+async function findBest() {
   let best = -Infinity;
   let bestP = null;
+  const perm = permutations([0, 1, 2, 3, 4]);
   for (const p of perm) {
-    const value = amp(p, 0);
+    const value = await amp(p, 0);
     if (value > best) {
       best = value;
       bestP = p;
     }
   }
   console.log("best", best, bestP);
-})();
+}
 
-function amp(phases, value) {
-  phases.forEach(phase => {
-    value = runProgram(program, [phase, value]);
-  });
-  //   console.log(`=== amp: ${value} ===`);
+async function amp(phases, value) {
+  const queues = phases.map(phase => new Queue([phase]));
+  queues[0].push(value);
+  queues.push(new Queue());
+  for (let i = 0; i < phases.length; i++) {
+    value = await runProgram(program, queues[i], queues[i + 1]);
+  }
   return value;
 }
-
-function permutations(list) {
-  if (list.length < 2) return [list];
-
-  const result = [];
-  list.forEach((value, index) => {
-    const rest = list.filter((x, i) => i !== index);
-    const perm = permutations(rest);
-    perm.forEach(p => {
-      result.push([value, ...p]);
-    });
-  });
-  return result;
-}
-
-function runProgram(program, inputs) {
+async function runProgram(program, inQueue, outQueue) {
   let p = 0;
-  let i = 0;
   let output;
   const readDirect = () => program[p++];
   const readIndirect = () => program[program[p++]];
@@ -76,14 +65,17 @@ function runProgram(program, inputs) {
       const b = read(mode[1]);
       write(a * b);
     },
-    3: () => {
+    3: async () => {
       // write input => program
-      const input = inputs[i++];
+      // const input = inputs[i++];
+      const input = await inQueue.pop();
+      // console.log("input", input);
       write(input);
     },
     4: () => {
       // read program => output
       output = readIndirect();
+      outQueue.push(output);
       //   console.log(`=== output: ${output} ===`);
     },
     5: mode => {
@@ -126,6 +118,6 @@ function runProgram(program, inputs) {
     if (op === 99) {
       return output;
     }
-    op_codes[op](mode);
+    await op_codes[op](mode);
   }
 }
--- src/util.js
+++ src/util.js
@@ -5,4 +5,42 @@ function readInput(folder, filename = "input.txt") {
   return fs.readFileSync(path.join(folder, filename)).toString();
 }
 
-module.exports = { readInput };
+function permutations(list) {
+  if (list.length < 2) return [list];
+
+  const result = [];
+  list.forEach((value, index) => {
+    const rest = list.filter((x, i) => i !== index);
+    const perm = permutations(rest);
+    perm.forEach(p => {
+      result.push([value, ...p]);
+    });
+  });
+  return result;
+}
+
+class Queue {
+  constructor(init = []) {
+    this.queue = [...init];
+    this.popPromise = null;
+  }
+
+  push(value) {
+    if (this.pending) {
+      this.pending(value);
+    }
+    this.queue.push(value);
+  }
+
+  async pop() {
+    if (this.queue.length > 0) {
+      return Promise.resolve(this.queue.shift());
+    } else {
+      return new Promise(resolve => {
+        this.popPromise = resolve;
+      });
+    }
+  }
+}
+
+module.exports = { readInput, permutations, Queue };
