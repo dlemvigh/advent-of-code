568aeb28dbe0268204de9bafb873cf9fb98e0870
David Lemvigh <dle@siteimprove.com>
2019-12-13 23:08:24 +0100

    wip

---
+++ src/2019/day5/test.js
@@ -0,0 +1,14 @@
+const { Program } = require("../intcode2");
+// const { part1, part2 } = require("./code");
+
+describe("day 2", () => {
+  describe("examples", () => {});
+
+  describe("part 1", () => {
+    it("verify solution", () => {});
+  });
+
+  describe("part 2", () => {
+    it("verify solution", () => {});
+  });
+});
