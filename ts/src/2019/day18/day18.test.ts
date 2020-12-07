1b8f5ce32a33e1790cda38089739522ea68e10c2
David Lemvigh <dlemvigh@gmail.com>
2020-12-07 15:46:52 +0100

    Day 18 input and todo tests

---
+++ ts/src/2019/day18/day18.test.ts
@@ -0,0 +1,12 @@
+describe("part 1", () => {
+  it.todo("small sample");
+  it.todo("larger sample");
+  it.todo("sample 3");
+  it.todo("sample 4");
+  it.todo("sample 5");
+  it.todo("solution");
+});
+
+describe("part 2", () => {
+    it.todo("small sample")
+})
