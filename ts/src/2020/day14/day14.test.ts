07dd8ecde43dd45b3bd314c290b113d5ede0ec21
David Lemvigh <dle@siteimprove.com>
2020-12-14 13:42:31 +0100

    Day 14 part 2

--- ts/src/2020/day14/day14.test.ts
+++ ts/src/2020/day14/day14.test.ts
@@ -1,22 +1,39 @@
 import { readInput } from "../../util";
-import { applyMask, part1 } from "./day14";
+import { applyMask, part1, part2 } from "./day14";
 
-describe("part 1", ()=>{
-	it("example 1", ()=>{
-		const mask = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X";
-			expect(applyMask(mask, 11)).toBe(73)
-			expect(applyMask(mask, 101)).toBe(101)
-			expect(applyMask(mask, 0)).toBe(64)
-	})
+describe("part 1", () => {
+  it("example 1", () => {
+    const mask = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X";
+    expect(applyMask(mask, 11)).toBe(73);
+    expect(applyMask(mask, 101)).toBe(101);
+    expect(applyMask(mask, 0)).toBe(64);
+  });
 
-	it("sample", ()=>{
-		const input = readInput(__dirname, "sample.txt");
-		const res = part1(input);
-		expect(res).toBe(165)
-	})
-	it("solution", ()=>{
-		const input = readInput(__dirname);
-		const res = part1(input);
-		expect(res).toBe(165)
-	})
-})
+  it("sample", () => {
+    const input = readInput(__dirname, "sample.txt");
+    const res = part1(input);
+    expect(res).toBe(165);
+  });
+  it("solution", () => {
+    const input = readInput(__dirname);
+    const res = part1(input);
+    expect(res).toBe(17934269678453);
+  });
+});
+
+describe("part 2", () => {
+  it("sample 1", () => {
+    const input = `mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1`;
+    const res = part2(input);
+    expect(res).toBe(208);
+  });
+
+  it("solution", () => {
+    const input = readInput(__dirname);
+		const res = part2(input);
+		expect(res).toBe(3440662844064);
+  });
+});
--- ts/src/2020/day14/day14.ts
+++ ts/src/2020/day14/day14.ts
@@ -38,7 +38,7 @@ export function part1(input: string): number {
 			// console.log("assign", addr, value);
 			const calced = calcMaskedValue(value, mask1, mask0)
 			if (addr === 34) {
-				console.log(addr, value, mask1, mask0, calced);
+				// console.log(addr, value, mask1, mask0, calced);
 				
 			}
 			memory[addr] = calcMaskedValue(value, mask1, mask0);
@@ -47,3 +47,58 @@ export function part1(input: string): number {
 	const sum = Object.values(memory).reduce((sum, val) => sum + val, 0);
 	return sum;
 }
+
+export function part2(input: string): number {
+	const memory: {[addr: number]: number} = {};
+	let mask: string;
+	const lines = splitIntoLines(input);
+	lines.forEach(line => {
+		if (rMask.test(line)) {
+			mask = line;
+			// console.log("mask", mask)
+		} else {
+			const mAssign = line.match(rAssign);
+			const addr = Number(mAssign[1])
+			const value = Number(mAssign[2])
+			const addrs = maskAddr(mask, addr)
+			addrs.forEach(x => memory[x] = value);
+		}
+
+	})
+	const sum = Object.values(memory).reduce((sum, val) => sum + val, 0);
+	return sum;
+}
+
+export function maskAddr(mask: string, addr: number): number[] {
+	const result = [];
+	const bits = addr.toString(2);
+	
+	const getBit = index => bits[bits.length - 1 - index] || "0";
+
+	for (let i = 0; i < mask.length; i++) {
+		const m = mask[mask.length - 1 - i];
+		
+		if (m === "0") result[i] = getBit(i);
+		else result[i] = m;
+		// console.log("it", i, m, getBit(i), "=", result[i]);
+	}
+	const resultMask = result.reverse().join("");
+
+	let addrs = [0]
+	for (let i = 0; i < resultMask.length; i++) {
+		const bit = resultMask[resultMask.length - 1 - i];
+		if (bit === "1") {
+			addrs = addrs.map(addr => addr + Math.pow(2, i))
+		}
+		if (bit === "X") {
+			addrs = [
+				...addrs,
+				...addrs.map(addr => addr + Math.pow(2, i))
+			];
+		}
+	}
+	return addrs;
+}
+
+
+
