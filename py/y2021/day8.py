23efd46932c2ef3ad861a19b753c9919625969be
David Lemvigh <dlemvigh@gmail.com>
2021-12-15 23:35:29 +0100

    day 8 part 2

--- py/y2021/day8.py
+++ py/y2021/day8.py
@@ -8,19 +8,105 @@ dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbc
 bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
 egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
 gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce"""
+
+# input = """acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf"""
+
 input = open("day8.txt", "r").read()
 
 lines = input.split("\n")
 
 def parse_line(line):
-	return [part.split(" ") for part in line.split(" | ")]
+	return [
+		["".join(sorted(word)) for word in part.split(" ")]
+		for part in line.split(" | ")
+	]
 
-unique_lengths = [2, 4, 3, 7]
-unique_count = 0
-for line in lines:
-	signals, outputs = parse_line(line)
-	print(signals, outputs)
-	for output in outputs:
-		if len(output)  in unique_lengths:
-			unique_count += 1
-print("unique count", unique_count)
+
+def part1(lines):
+	unique_lengths = [2, 4, 3, 7]
+	unique_count = 0
+
+	for line in lines:
+		signals, outputs = parse_line(line)
+		for output in outputs:
+			if len(output)  in unique_lengths:
+				unique_count += 1
+
+	return unique_count
+
+def part2(lines):
+	total = 0
+	for line in lines:
+		signals, outputs = parse_line(line)
+
+		lookup = build_lookup(signals)
+		res = int("".join([str(lookup[key]) for key in outputs]))
+		print(res)
+		total += res
+	return total
+
+def build_lookup(signals):
+	lookup = {}
+
+	# find 1
+	key = find_one_by_length(signals, 2)
+	add_lookup(lookup, key, 1)
+	# find 7
+	key = find_one_by_length(signals, 3)
+	add_lookup(lookup, key, 7)
+	# find 4
+	key = find_one_by_length(signals, 4)
+	add_lookup(lookup, key, 4)
+	# find 8
+	key = find_one_by_length(signals, 7)
+	add_lookup(lookup, key, 8)
+
+	fives = [x for x in signals if len(x) == 5]
+	sixes = [x for x in signals if len(x) == 6]	
+
+	# find 3
+	key = find_one_by_overlap(fives, lookup[7])
+	add_lookup(lookup, key, 3)
+	fives.remove(key)
+
+	# find 9
+	key = find_one_by_overlap(sixes, lookup[3])
+	add_lookup(lookup, key, 9)
+	sixes.remove(key)
+
+	# find 0
+	key = find_one_by_overlap(sixes, lookup[7])
+	add_lookup(lookup, key, 0)
+	sixes.remove(key)
+
+	# find 6
+	add_lookup(lookup, sixes[0], 6)
+
+	# find 5
+	key = find_one_by_is_overlapped(fives, lookup[6])
+	add_lookup(lookup, key, 5)
+	fives.remove(key)
+
+	# find 2
+	add_lookup(lookup, fives[0], 2)
+
+	return lookup
+
+def add_lookup(lookup, key, value):
+	lookup[key] = value
+	lookup[value] = key
+
+def find_one_by_length(list, length):
+	return next(x for x in list if len(x) == length)
+
+def find_one_by_overlap(list, value):
+	return next(x for x in list if overlaps(value, x))
+
+def find_one_by_is_overlapped(list, value):
+	return next(x for x in list if overlaps(x, value))
+
+def overlaps(a, b):
+	return len(set(a) & set(b)) == len(a)
+
+# print("unique count", part1(lines))
+print("total", part2(lines))
