b092043c964850ecda0fb95bb68c025350dfc652
David Lemvigh <dlemvigh@gmail.com>
2022-12-04 08:16:50 +0100

    Day 4 part 1

---
+++ vite/src/day4/day4.test.ts
@@ -0,0 +1,56 @@
+import { describe, expect, it } from "vitest";
+import { readInput } from "../util";
+import { CleanRangePair, isFullyContained, parseLine, part1 } from "./day4";
+
+
+
+describe("day 4", ()=>{
+    describe("parse line", ()=>{
+        type TestCase = [string, CleanRangePair];
+        const testCases: TestCase[] = [
+            ["1-2,3-4", [[1, 2], [3, 4]]],
+            ["12-34,56-78", [[12, 34], [56, 78]]],
+        ]
+        testCases.forEach(([input, expected]) =>{
+            it(input, ()=>{
+                expect(parseLine(input)).toEqual(expected)
+            })
+        })
+    })
+
+    describe("is fully contained",()=>{
+        type TestCase = [CleanRangePair, boolean];
+        const testCases: TestCase[] = [
+            [[[1,2],[3,4]], false],
+            [[[3,4],[1,2]], false],
+            [[[1,4],[2,3]], true],
+            [[[2,3],[1,4]], true],
+            [[[1,1],[1,1]], true],
+            [[[1,123],[1,123]], true],
+            [[[12,45],[23,34]], true],
+            [[[1,3],[2,4]], false],
+            [[[1,3],[0,2]], false]
+        ]
+        testCases.forEach(([pair, expected])=>{
+            it(JSON.stringify(pair),()=>{
+                expect(isFullyContained(pair)).toBe(expected)
+            })
+        })
+    })
+
+    describe("part 1",()=>{
+        it("sample",()=>{
+            const sample = `2-4,6-8
+                2-3,4-5
+                5-7,7-9
+                2-8,3-7
+                6-6,4-6
+                2-6,4-8`;
+            expect(part1(sample)).toBe(2)
+        });
+        it("input",()=>{
+            const input = readInput(__dirname, "input.txt");
+            expect(part1(input)).toBe(518)
+        })
+    })
+})
