ab5d8d1ab783dff7ed0a12350cf07a3474604a9d
David Lemvigh <dle@siteimprove.com>
2022-12-05 16:46:47 +0100

    Day 2

--- vite/src/day1/day1.test.ts
+++ vite/src/day1/day1.test.ts
@@ -21,42 +21,44 @@ function part2(input: string): number {
   return sum(top3);
 }
 
-describe("part 1", () => {
-  it("sample", () => {
-    // arrange
-    const input = readInput(__dirname, "sample.txt");
-    // act
-    const result = part1(input);
-    // assert
-    expect(result).toBe(24000);
+describe("day 1", () => {
+  describe("part 1", () => {
+    it("sample", () => {
+      // arrange
+      const input = readInput(__dirname, "sample.txt");
+      // act
+      const result = part1(input);
+      // assert
+      expect(result).toBe(24000);
+    });
+
+    it("input", () => {
+      // arrange
+      const input = readInput(__dirname, "input.txt");
+      // act
+      const result = part1(input);
+      // assert
+      expect(result).toBe(70374);
+    });
   });
 
-  it("input", () => {
-    // arrange
-    const input = readInput(__dirname, "input.txt");
-    // act
-    const result = part1(input);
-    // assert
-    expect(result).toBe(70374);
-  });
-});
+  describe("part 2", () => {
+    it("sample", () => {
+      // arrange
+      const input = readInput(__dirname, "sample.txt");
+      // act
+      const result = part2(input);
+      // assert
+      expect(result).toBe(45000);
+    });
 
-describe("part 2", () => {
-  it("sample", () => {
-    // arrange
-    const input = readInput(__dirname, "sample.txt");
-    // act
-    const result = part2(input);
-    // assert
-    expect(result).toBe(45000);
-  });
-
-  it("input", () => {
-    // arrange
-    const input = readInput(__dirname, "input.txt");
-    // act
-    const result = part2(input);
-    // assert
-    expect(result).toBe(204610);
+    it("input", () => {
+      // arrange
+      const input = readInput(__dirname, "input.txt");
+      // act
+      const result = part2(input);
+      // assert
+      expect(result).toBe(204610);
+    });
   });
 });
