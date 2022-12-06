e2b99c6c482d2d22d9297ca9a621242c4c2a4553
David Lemvigh <dlemvigh@gmail.com>
2022-12-06 18:14:36 +0100

    Math utilities

--- vite/src/math.test.ts
+++ vite/src/math.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { sum } from "./math";
+import { sum, sumBy, average, averageBy } from "./math";
 
 describe("sum", () => {
   type TestCase = [number[], number]; // [input, expected]
@@ -22,3 +22,65 @@ describe("sum", () => {
     });
   });
 });
+
+describe("sum by", () => {
+  it("sum by numbers", ()=>{
+    expect(sumBy([], x => x)).toBe(0);
+    expect(sumBy([1], x => x)).toBe(1);
+    expect(sumBy([1,2,3], x => x)).toBe(6);
+  })
+
+  it("sum by object", ()=>{
+    type TestObject = { a: number, b: number };
+    const list: TestObject[] = [
+      { a: 1, b: 2 },
+      { a: 1, b: 2 },
+      { a: 1, b: 2 },
+    ]
+
+    expect(sumBy(list, x => x.a)).toBe(3);
+    expect(sumBy(list, x => x.b)).toBe(6);
+    expect(sumBy(list, () => 3)).toBe(9);
+  })
+})
+
+describe("average", ()=>{
+  type TestCase = [number[], number];
+  const test_cases: TestCase[] = [
+    [[0], 0],
+    [[1], 1],
+    [[2], 2],
+    [[-1], -1],
+    [[0.5], 0.5],
+    [[1, 2, 3], 2],
+    [[1, -1], 0],
+    [[1, -2, 3, -4, 5, -6], -.5],
+    [[0.1, 0.2, 0.3], 0.2],
+  ];
+
+  test_cases.forEach(([input, expected]) => {
+    it(`[${input.join(", ")}] = ${expected}`, () => {
+      expect(average(input)).toBeCloseTo(expected, 10);
+    });
+  });
+})
+
+describe("average by", () => {
+  it("average by numbers", ()=>{
+    expect(averageBy([1], x => x)).toBe(1);
+    expect(averageBy([1,2,3], x => x)).toBe(2);
+  })
+
+  it("average by object", ()=>{
+    type TestObject = { a: number, b: number };
+    const list: TestObject[] = [
+      { a: 1, b: 2 },
+      { a: 1, b: 2 },
+      { a: 1, b: 2 },
+    ]
+
+    expect(averageBy(list, x => x.a)).toBe(1);
+    expect(averageBy(list, x => x.b)).toBe(2);
+    expect(averageBy(list, () => 3)).toBe(3);
+  })
+})
--- vite/src/math.ts
+++ vite/src/math.ts
@@ -1,3 +1,19 @@
 export function sum(numbers: number[]): number {
   return numbers.reduce((a, b) => a + b, 0);
 }
+
+export function sumBy<T>(list: T[], selector: (item: T) => number): number {
+  return list.reduce((sum, item) => {
+    const value = selector(item);
+    return sum + value
+  }, 0)
+}
+
+export function average(numbers: number[]): number {
+  return sum(numbers) / numbers.length;
+}
+
+export function averageBy<T>(list: T[], selector: (item: T) => number): number {
+  return sumBy(list, selector) / list.length;
+}
+
