257637d7cc2e27f88756906f0599a1907cf7bf8a
David Lemvigh <david.lemvigh@awaze.com>
2023-10-19 17:07:37 +0200

    Year 2020 Day 17 Part 1 - csharp

---
+++ cs/AdventOfCode.Tests/Y2020/Day17Tests.cs
@@ -0,0 +1,95 @@
+﻿using AdventOfCode.Y2020;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Tests.Y2020
+{
+    public class Day17Tests
+    {
+        private readonly Day17 sut = new Day17();
+
+        [Theory]
+        [FileTestData("Y2020/Day17/sample.in",112)]
+        [FileTestData("Y2020/Day17/input.in", 230)]
+        public void Part1(string input, int expected)
+        {
+            Assert.Equal(expected, sut.Part1(input));
+        }
+
+        [Theory]
+        [FileTestData("Y2020/Day17/sample.in")]
+        [FileTestData("Y2020/Day17/input.in")]
+        public void Part1_ToString(string input)
+        {
+            var cube = sut.ParseInput(input);
+            Assert.Equal(input, cube.ToString());
+        }
+
+        [Theory]
+        [FileTestData("Y2020/Day17/sample.in", 0, 0, 0, false)]
+        [FileTestData("Y2020/Day17/sample.in", 0, 0, 1, true)]
+        [FileTestData("Y2020/Day17/sample.in", 0, 0, 2, false)]
+        [FileTestData("Y2020/Day17/sample.in", 0, 1, 0, false)]
+        [FileTestData("Y2020/Day17/sample.in", 0, 1, 1, false)]
+        [FileTestData("Y2020/Day17/sample.in", 0, 1, 2, true)]
+        [FileTestData("Y2020/Day17/sample.in", 0, 2, 0, true)]
+        [FileTestData("Y2020/Day17/sample.in", 0, 2, 1, true)]
+        [FileTestData("Y2020/Day17/sample.in", 0, 2, 2, true)]
+        public void Part1_Index(string input, int z, int y, int x, bool expected)
+        {
+            var cube = sut.ParseInput(input);
+            var actual = cube[z, y, x];
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Part1_GetIndex()
+        {
+            var cube = new Day17.Cube(2, 3, 7);
+            var index = cube.GetIndex(1, 2, 6);
+            Assert.Equal(1 * 3 * 7 + 2 * 7 + 6, index);
+        }
+
+        [Theory]
+        [FileTestData("Y2020/Day17/sample.in")]
+        public void Part1_GetNextGeneration(string input)
+        {
+            var cube = sut.ParseInput(input);
+            var next = sut.GetNextGeneration(cube);
+            var expected = new[]
+            {
+                new [] {
+                    ".....",
+                    ".....",
+                    ".#...",
+                    "...#.",
+                    "..#.."
+                },
+                new [] {
+                    ".....",
+                    ".....",
+                    ".#.#.",
+                    "..##.",
+                    "..#.."
+                },
+                new [] {
+                    ".....",
+                    ".....",
+                    ".#...",
+                    "...#.",
+                    "..#.."
+                }
+            };
+            var expectedStr = string.Join("\n\n", expected.Select(layer => string.Join("\n", layer)));
+            var actual = next.ToString();
+            
+            Assert.Equal(expectedStr, actual);
+        }
+
+
+    }
+}
---
+++ cs/AdventOfCode/Y2020/Day17.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Y2020
+{
+    public class Day17
+    {
+        public int Part1(string input)
+        {
+            var cube = ParseInput(input);
+            cube = GetNextGeneration(cube, 6);
+            var count = GetActiveCount(cube);
+            return count;
+        }
+
+        public Cube ParseInput(string input)
+        {
+            var lines = input.Split('\n');
+
+            var layers = 1;
+            var rows = lines.Length;
+            var columns = lines.First().Length;
+
+            var cube = new Cube(layers, rows, columns);
+
+            for (int row = 0; row < rows; row++)
+            {
+                var line = lines[row];
+                for (int column = 0; column < line.Length; column++)
+                {
+                    var active = line[column] == '#';
+                    cube[0, row, column] = active;
+                }
+            }
+
+            return cube;
+        }
+
+        public int GetActiveCount(Cube cube)
+        {
+            var count = 0;
+
+            cube.ForEach((value, index) =>
+            {
+                if (value)
+                {
+                    count++;
+                }
+            });
+
+            return count;
+        }
+
+        public IEnumerable<(int layer, int row, int column)> GetNear((int layer, int row, int column) index)
+        {
+            for (int layer = index.layer - 1; layer <= index.layer + 1; layer++)
+            {
+                for (int row = index.row - 1; row <= index.row + 1; row++)
+                {
+                    for (int column = index.column - 1; column <= index.column + 1; column++)
+                    {
+                        if (column == index.column && row == index.row && layer == index.layer) continue;
+                        yield return (layer, row, column);
+                    }
+                }
+            }
+        }
+
+        public Cube GetNextGeneration(Cube prev)
+        {
+            var next = new Cube(prev.layers+ 2, prev.rows + 2, prev.columns + 2);
+            next.ForEach((_, index) =>
+            {
+                next[index.layer, index.row, index.column] = GetNext(index, prev);
+            });
+            return next;
+        }
+
+        public Cube GetNextGeneration(Cube cube, int count)
+        {
+            if (count <= 0) throw new ArgumentException("Generation count must be greater than zero", nameof(count));
+
+            for (var it = 0; it < count; it++)
+            {
+                cube = GetNextGeneration(cube);
+            }
+
+            return cube;
+        }
+
+        public bool GetNext((int layer, int row, int column) index, Cube prev)
+        {
+            var value = prev[index.layer - 1, index.row - 1, index.column - 1];
+            var near = GetNear(index).Count(index => prev[index.layer - 1, index.row - 1, index.column - 1]);
+            var active = (value && near == 2) || near == 3;
+            return active;
+        }
+
+        public class Cube
+        {
+            public readonly int columns;
+            public readonly int rows;
+            public readonly int layers;
+
+            private readonly bool[,,] data;
+
+            public Cube(int layers, int rows, int columns)
+            {
+                if (layers <= 0) throw new ArgumentException("Size must be greater than zero", nameof(layers));
+                if (rows <= 0) throw new ArgumentException("Size must be greater than zero", nameof(rows));
+                if (columns <= 0) throw new ArgumentException("Size must be greater than zero", nameof(columns));
+
+                this.layers = layers;
+                this.rows = rows;
+                this.columns = columns;
+
+                data = new bool[layers, rows, columns];
+            }
+
+            public override string ToString()
+            {
+                var layers = Enumerable.Range(0, this.layers).Select(layer =>
+                {
+                    var rows = Enumerable.Range(0, this.rows).Select(row =>
+                    {
+                        var cols = Enumerable.Range(0, this.columns).Select(column =>
+                        {
+                            return this.data[layer, row, column] ? "#" : ".";
+                        });
+                        return string.Join("", cols);
+                    });
+                    return string.Join("\n", rows);
+                });
+                return string.Join("\n\n", layers);
+            }
+
+            public int GetIndex(int layer, int row, int column)
+            {
+                return (((0 * this.layers) + layers) * this.rows + rows) * this.columns + columns;
+            }
+
+            public bool this[int layer, int row, int column]
+            {
+                get {
+                    if (layer < 0 || layer >= this.layers) return false;
+                    if (row < 0 || row >= this.rows) return false;
+                    if (column < 0 || column >= this.columns) return false;
+
+                    return data[layer, row, column];
+                }
+                set { data[layer, row, column] = value; }
+            }
+
+            public void ForEach(Action<bool, (int layer, int row, int column)> action)
+            {
+                for(int layer = 0; layer < this.layers; layer++)
+                {
+                    for (int row = 0; row < this.rows; row++)
+                    {
+                        for (int column = 0; column < this.columns; column++)
+                        {
+                            var value = data[layer, row, column];
+                            var index = (layer, row, column);
+                            action(value, index);
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
