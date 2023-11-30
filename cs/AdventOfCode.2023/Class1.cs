bd6d4e83dfa10b8b1213c35ee0726b23ccc46016
David Lemvigh <david.lemvigh@awaze.com>
2023-11-30 14:09:57 +0100

    Add template for 2023

--- cs/AdventOfCode.2023/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace AdventOfCode._2023;
-
-public class Class1
-{
-
-}
---
+++ cs/AdventOfCode.2023/DayTemplate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCode.Shared;
+
+namespace AdventOfCode.Y2022
+{
+    [ProblemName("")]
+    public class DayTemplate
+    {
+        public int Part1(string input)
+        {
+            var parsed = ParseInput(input);
+            return parsed.First();
+            throw new NotImplementedException();
+        }
+
+        public int Part2(string input)
+        {
+            var parsed = ParseInput(input);
+            return parsed.Last();
+            throw new NotImplementedException();
+
+        }
+        public IEnumerable<int> ParseInput(string input)
+        {
+            return input.Split("\n").Select(int.Parse);
+        }
+    }
+}
