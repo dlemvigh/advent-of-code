705d797834832c2db1f241dc27a1d681375311eb
David Lemvigh <dlemvigh@gmail.com>
2023-12-07 14:06:56 +0100

    Day 7 part 2

--- cs/AdventOfCode.2023.Tests/Day7Tests.cs
+++ cs/AdventOfCode.2023.Tests/Day7Tests.cs
@@ -11,10 +11,77 @@ namespace AdventOfCode2023.Tests
 {
     public class Day7Tests
     {
+        [Theory]
+        [InlineData('A', 14)]
+        [InlineData('K', 13)]
+        [InlineData('Q', 12)]
+        [InlineData('J', 11)]
+        [InlineData('T', 10)]
+        [InlineData('9', 9)]
+        [InlineData('8', 8)]
+        [InlineData('7', 7)]
+        [InlineData('6', 6)]
+        [InlineData('5', 5)]
+        [InlineData('4', 4)]
+        [InlineData('3', 3)]
+        [InlineData('2', 2)]
+        public void ParseCard(char card, int expected)
+        {
+            // arrange 
+            var sut = new Day7();
+
+            // act
+            var actual = sut.ParseCard(card);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("23456 42", new[] { 2, 3, 4, 5, 6 }, 42)]
+        [InlineData("TJQKA 0", new[] { 10, 11, 12, 13, 14 }, 0)]
+        public void ParseHand(string line, int[] expectedCards, int expectedBid)
+        {
+            // arrange 
+            var sut = new Day7();
+
+            // act
+            var actual = sut.ParseHand(line);
+
+            // assert
+            Assert.Equal(expectedCards, actual.Cards);
+            Assert.Equal(expectedBid, actual.Bid);
+        }
+
+        [Theory]
+        [InlineData(Day7.HandType.Five, 9, 9, 9, 9, 9)]
+        [InlineData(Day7.HandType.Four, 9, 9, 9, 9, 1)]
+        [InlineData(Day7.HandType.Four, 9, 9, 9, 1, 9)]
+        [InlineData(Day7.HandType.Four, 9, 9, 1, 9, 9)]
+        [InlineData(Day7.HandType.Four, 9, 1, 9, 9, 9)]
+        [InlineData(Day7.HandType.Four, 1, 9, 9, 9, 9)]
+        [InlineData(Day7.HandType.FullHouse, 1, 1, 1, 2, 2)]
+        [InlineData(Day7.HandType.Three, 1, 1, 1, 2, 3)]
+        [InlineData(Day7.HandType.TwoPairs, 1, 1, 2, 2, 3)]
+        [InlineData(Day7.HandType.TwoPairs, 1, 1, 2, 3, 3)]
+        [InlineData(Day7.HandType.TwoPairs, 1, 2, 2, 3, 3)]
+        [InlineData(Day7.HandType.OnePair, 1, 1, 2, 3, 4)]
+        [InlineData(Day7.HandType.HighCard, 1, 2, 3, 4, 5)]
+        public void GetHandType(Day7.HandType expected, params int[] cards)
+        {
+            // arrange 
+            var sut = new Day7();
+
+            // act
+            var actual = sut.GetHandType(cards);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
 
         [Theory]
         [FileTestData("Day7/sample.in", 6440)]
-        [FileTestData("Day7/input.in", 6440)]
+        [FileTestData("Day7/input.in", 253866470)] // 253866471 Too high
         public void Part1(string input, int expected)
         {
             // arrange 
@@ -27,19 +94,19 @@ namespace AdventOfCode2023.Tests
             Assert.Equal(expected, actual);
         }
 
-        //[Theory]
-        //[FileTestData("Day7/sample.in", 4)]
-        //[FileTestData("Day7/input.in", 41)]
-        //public void Part2(string input, int expected)
-        //{
-        //    // arrange 
-        //    var sut = new Day7();
+        [Theory]
+        [FileTestData("Day7/sample.in", 5905)]
+        [FileTestData("Day7/input.in", 254494947)]
+        public void Part2(string input, int expected)
+        {
+            // arrange 
+            var sut = new Day7();
 
-        //    // act
-        //    var actual = sut.Part2(input);
+            // act
+            var actual = sut.Part2(input);
 
-        //    // assert
-        //    Assert.Equal(expected, actual);
-        //}
+            // assert
+            Assert.Equal(expected, actual);
+        }
     }
 }
--- cs/AdventOfCode.2023/Day7.cs
+++ cs/AdventOfCode.2023/Day7.cs
@@ -12,21 +12,136 @@ namespace AdventOfCode.Y2022
     {
         public int Part1(string input)
         {
-            var parsed = ParseInput(input);
-            return parsed.First();
-            throw new NotImplementedException();
+            var hands = ParseInput(input);
+            var sorted = SortHands(hands);
+            var score = CalcScore(sorted);
+
+            return score;
         }
 
         public int Part2(string input)
         {
-            var parsed = ParseInput(input);
-            return parsed.Last();
-            throw new NotImplementedException();
+            var hands = ParseInputJackWild(input);
+            var sorted = SortHands(hands);
+            var score = CalcScore(sorted);
 
+            return score;
         }
-        public IEnumerable<int> ParseInput(string input)
+
+        public int CalcScore(IEnumerable<Hand> hands)
         {
-            return input.Split("\n").Select(int.Parse);
+            var scores = hands.Select((hand, index) => hand.Bid * (index + 1));
+            var score = scores.Sum();
+            return score;
+        }
+
+        public IEnumerable<Hand> SortHands(IEnumerable<Hand> hands)
+        {
+            return hands.OrderBy(x => -(int)x.HandType)
+                                .ThenBy(x => x.Cards[0])
+                                .ThenBy(x => x.Cards[1])
+                                .ThenBy(x => x.Cards[2])
+                                .ThenBy(x => x.Cards[3])
+                                .ThenBy(x => x.Cards[4]);
+        }
+
+        public HandType GetHandType(int[] cards)
+        {
+            var groups = cards.GroupBy(x => x).Select(x => x.Count());
+
+            if (groups.Any(x => x == 5)) return HandType.Five;
+            if (groups.Any(x => x == 4)) return HandType.Four;
+            if (groups.Any(x => x == 3) && groups.Any(x => x == 2)) return HandType.FullHouse;
+            if (groups.Any(x => x == 3)) return HandType.Three;
+            if (groups.Where(x => x == 2).Count() == 2) return HandType.TwoPairs;
+            if (groups.Any(x => x == 2)) return HandType.OnePair;
+
+            return HandType.HighCard;
+        }
+        public HandType GetHandTypeJackWild(int[] cards)
+        {
+            // convert jack to highest occorence card
+            var cardsWithoutJacks = cards.Where(x => x != 1);
+            var cardsOnlyJacks = cards.Where(x => x == 1);
+            var highestOccurenceCard = cardsWithoutJacks.GroupBy(x => x).MaxBy(x => x.Count());
+
+            var mappedJackCards = cardsOnlyJacks.Select(x => highestOccurenceCard?.Key ?? 1);
+            var mappedHand = cardsWithoutJacks.Concat(mappedJackCards).ToArray();
+
+            // GetHandType
+            return GetHandType(mappedHand);
+        }
+
+        public IEnumerable<Hand> ParseInput(string input)
+        {
+            return input.Split("\n").Select(ParseHand);
+        }
+
+        public Hand ParseHand(string line)
+        {
+            var parts = line.Split(" ");
+
+            var cards = parts[0].Select(ParseCard).ToArray();
+            var bid = int.Parse(parts[1]);
+            var type = GetHandType(cards);
+
+            return new Hand(cards, bid, type);
+        }
+
+        public int ParseCard(char card)
+        {
+            switch(card)
+            {
+                case 'A': return 14;
+                case 'K': return 13;
+                case 'Q': return 12;
+                case 'J': return 11;
+                case 'T': return 10;
+                default: return card - '0';
+            }
+        }
+
+        public IEnumerable<Hand> ParseInputJackWild(string input)
+        {
+            return input.Split("\n").Select(ParseHandJackWild);
+        }
+
+
+        public Hand ParseHandJackWild(string line)
+        {
+            var parts = line.Split(" ");
+
+            var cards = parts[0].Select(ParseCardJackWild).ToArray();
+            var bid = int.Parse(parts[1]);
+            var type = GetHandTypeJackWild(cards);
+
+            return new Hand(cards, bid, type);
+        }
+
+        public int ParseCardJackWild(char card)
+        {
+            switch (card)
+            {
+                case 'A': return 14;
+                case 'K': return 13;
+                case 'Q': return 12;
+                case 'J': return 1;
+                case 'T': return 10;
+                default: return card - '0';
+            }
+        }
+
+        public record Hand(int[] Cards, int Bid, HandType HandType);
+
+        public enum HandType
+        {
+            Five = 1,
+            Four = 2,
+            FullHouse = 3,
+            Three = 4,
+            TwoPairs = 5,
+            OnePair = 6,
+            HighCard = 7,
         }
     }
 }
