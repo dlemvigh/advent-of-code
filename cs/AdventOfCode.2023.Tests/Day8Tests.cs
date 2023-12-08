e75cdfceab5374ffcfc0cf0b33a6f12703fc8159
David Lemvigh <david.lemvigh@awaze.com>
2023-12-08 23:42:33 +0100

    Day 8

---
+++ cs/AdventOfCode.2023.Tests/Day8Tests.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCode.Tests;
+using AdventOfCode.Y2022;
+using AdventOfCode2023;
+
+namespace AdventOfCode2023.Tests
+{
+    public class Day8Tests
+    {
+
+        [Theory]
+        [FileTestData("Day8/sample1.in", 2)]
+        [FileTestData("Day8/sample2.in", 6)]
+        [FileTestData("Day8/input.in", 18157)]
+        public void Part1(string input, int expected)
+        {
+            // arrange 
+            var sut = new Day8();
+
+            // act
+            var actual = sut.Part1(input);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [FileTestData("Day8/sample3.in", 6L)]
+        [FileTestData("Day8/input.in", 14299763833181L)]
+        public void Part2(string input, long expected)
+        {
+            // arrange 
+            var sut = new Day8();
+
+            // act
+            var actual = sut.Part2(input);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ cs/AdventOfCode.2023/Day8.cs
@@ -0,0 +1,174 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using AdventOfCode.Shared;
+
+namespace AdventOfCode.Y2022
+{
+    [ProblemName("")]
+    public class Day8
+    {
+        public int Part1(string input)
+        {
+            var (steps, nodes) = ParseInput(input);
+            var count = FollowSteps(steps, nodes, "AAA", "ZZZ");
+            return count;
+        }
+
+        public long Part2(string input)
+        {
+            var (steps, nodes) = ParseInput(input);
+            var count = FollowGhostSteps(steps, nodes, "A", "Z");
+            return count;
+
+        }
+
+        public int FollowSteps(string steps, Dictionary<string, Node> nodes, string src, string dest)
+        {
+            var count = 0;
+            var node = nodes[src];
+
+            while (node.Name != dest)
+            {
+                if (steps[count % steps.Length] == 'L')
+                {
+                    node = node.Left;
+                }
+                else
+                {
+                    node = node.Right;
+                }
+                count++;
+            }
+
+            return count;
+        }
+
+
+        public long GCD(long a, long b)
+        {
+            if (b == 0) return a;
+            return GCD(b, a % b);
+        }
+
+
+        public long GetLoopSize(string steps, Node node)
+        {
+            var visited = new HashSet<Node>();
+
+            int tailIndex = 0;
+            while (!visited.Contains(node) )
+            {
+                visited.Add(node);
+                foreach (var step in steps)
+                {
+                    node = Step(node, step);
+                    tailIndex++;
+                }
+            }
+
+            var loopNode = node;
+            var loopIndex = 0;
+            while (node != loopNode || loopIndex == 0)
+            {
+                foreach (var step in steps)
+                {
+                    node = Step(node, step);
+                    loopIndex++;
+                }
+            }
+
+
+            return loopIndex;
+        }
+
+        public Node Step(Node node, string steps, int step)
+        {
+            return Step(node, steps[step % steps.Length]);
+        }
+
+        public Node Step(Node node, char step)
+        {
+            if (step == 'L')
+            {
+                return node.Left;
+            }
+            else
+            {
+                return node.Right;
+            }
+        }   
+
+        public long FollowGhostSteps(string steps, Dictionary<string, Node> nodes, string src, string dest)
+        {
+            var ghosts = nodes.Where(x => x.Value.Name.EndsWith(src)).Select(x => x.Value).ToArray();
+
+            var loops = new Queue<long>(ghosts.Select(ghost => GetLoopSize(steps, ghost)));
+
+            while (loops.Count() > 1)
+            {
+                var a = loops.Dequeue();
+                var b = loops.Dequeue();
+                var gcd = GCD(a, b);
+                var lcm = a * b / gcd;
+                loops.Enqueue(lcm);
+            }
+
+            return loops.Dequeue();
+        }
+
+        public (string steps, Dictionary<string, Node> nodes) ParseInput(string input)
+        {
+            var lines = input.Split("\n");
+            var steps = lines[0];
+
+            var nodes = ParseNodes(lines.Skip(2));
+
+            return (steps, nodes);
+        }
+
+        public Dictionary<string, Node> ParseNodes(IEnumerable<string> lines)
+        {
+            var nodes = new Dictionary<string, Node>();
+            var parsed = lines.Select(ParseNode);
+
+            foreach (var info in parsed)
+            {
+               nodes.Add(info.name, new Node(info.name));
+            }
+            foreach (var info in parsed)
+            {
+                var node = nodes[info.name];
+                node.Left = nodes[info.left];
+                node.Right = nodes[info.right];
+            }
+
+            return nodes;
+        }
+
+        public (string name, string left, string right) ParseNode(string line)
+        {
+            // parse line with format "AAA = (BBB, CCC)" into name = (left, right)
+            var match = Regex.Match(line, @"(\w+) = \((\w+), (\w+)\)");
+            var name = match.Groups[1].Value;
+            var left = match.Groups[2].Value;
+            var right = match.Groups[3].Value;
+            return (name, left, right);
+        }
+
+        public class Node
+        {
+            public string Name { get; init; }
+            public Node Left { get; set; }
+            public Node Right { get; set; }
+
+            public Node(string name)
+            {
+                Name = name;
+            }
+        }
+    }
+}
