38a19022db345e27c4c2c2f1b466948d7e8f84b0
David Lemvigh <dlemvigh@gmail.com>
2023-12-09 11:42:29 +0100

    Add concrete instructions

--- cs/AdventOfCode.2019/Intcode/Models/Instruction.cs
+++ cs/AdventOfCode.2019/Intcode/Models/Instruction.cs
@@ -8,4 +8,77 @@ namespace AdventOfCode2019.Intcode.Models
 {
     public record Instruction(Op op, Arg[] inputs, Arg? output = null);
     public record Arg(long adr, Mode mode);
+
+    public abstract record InstructionNoArgs 
+    {
+        public Op Op { get; init; }
+
+        protected InstructionNoArgs(Op op)
+        {
+            Op = op;
+        }
+    }
+
+    public abstract record InstructionOneArg : InstructionNoArgs
+    {
+        public Arg A { get; init; }
+
+        protected InstructionOneArg(Op op, Arg a) : base(op)
+        {
+            A = a;
+        }
+    }
+
+    public abstract record InstructionTwoArgs : InstructionOneArg
+    {
+        public Arg B { get; init; }
+
+        protected InstructionTwoArgs(Op op, Arg a, Arg b) : base(op, a)
+        {
+            B = b;
+        }
+    }
+
+    public abstract record InstructionThreeArgs : InstructionTwoArgs
+    {
+        public Arg C { get; init; }
+
+        protected InstructionThreeArgs(Op op, Arg a, Arg b, Arg c) : base(op, a, b)
+        {
+            C = c;
+        }
+    }
+
+    public record AddInstruction : InstructionThreeArgs
+    {
+        public AddInstruction(Arg a, Arg b, Arg c) : base(Op.ADD, a, b, c) { }
+    }
+    public record MultiplyInstruction : InstructionThreeArgs
+    {
+        public MultiplyInstruction(Arg a, Arg b, Arg c) : base(Op.MUL, a, b, c) { }
+    }
+    public record InputInstruction : InstructionOneArg {
+        public InputInstruction(Arg a) : base(Op.IN, a) {}
+    }
+    public record OutputInstruction : InstructionOneArg {
+        public OutputInstruction(Arg a) : base(Op.OUT, a) {}
+    }
+    public record JumpTrueInstruction : InstructionTwoArgs {
+        public JumpTrueInstruction(Arg a, Arg b) : base(Op.JUMP_TRUE, a, b) {}
+    }
+    public record JumpFalseInstruction : InstructionTwoArgs {
+        public JumpFalseInstruction(Arg a, Arg b) : base(Op.JUMP_FALSE, a, b) {}
+    }
+    public record LessThanInstruction : InstructionThreeArgs {
+        public LessThanInstruction(Arg a, Arg b, Arg c) : base(Op.LESS, a, b, c) { }
+    }
+    public record EqualToInstruction : InstructionThreeArgs {
+        public EqualToInstruction(Arg a, Arg b, Arg c) : base(Op.EQUAL, a, b, c) { }
+    }
+    public record AdjustRelativeBaseInstruction : InstructionOneArg {
+        public AdjustRelativeBaseInstruction(Arg a) : base(Op.IN, a) {}
+    }
+    public record HaltInstruction : InstructionNoArgs {
+        public HaltInstruction() : base(Op.HALT) {}
+    }
 }
