d473409ef8de7a9bb709c3c873d37edc7051011f
David Lemvigh <dlemvigh@gmail.com>
2023-12-16 10:27:05 +0100

    Day16 part 1

---
+++ cs/AdventOfCode.2023.Tests/Day16Tests.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCode.Tests;
+using AdventOfCode.Y2022;
+using AdventOfCode2023;
+
+namespace AdventOfCode2023.Tests
+{
+    public class Day16Tests
+    {
+        [Theory]
+        [InlineData(0, 0, Day16.Dir.Up, -1, 0)]
+        [InlineData(0, 0, Day16.Dir.Down, 1, 0)]
+        [InlineData(0, 0, Day16.Dir.Left, 0, -1)]
+        [InlineData(0, 0, Day16.Dir.Right, 0, 1)]
+        public void GetNextPos(int row, int col, Day16.Dir dir, int expectedRow, int expectedCol)
+        {
+            // arrange 
+            var sut = new Day16();
+            var expected = (expectedRow, expectedCol);
+
+            // act
+            var actual = sut.GetNextPos(row, col, dir);
+
+            // assert
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(Day16.Tile.Empty, Day16.Dir.Up, new Day16.Dir[] { Day16.Dir.Up })]
+        [InlineData(Day16.Tile.Empty, Day16.Dir.Down, new Day16.Dir[] { Day16.Dir.Down })]
+        [InlineData(Day16.Tile.Empty, Day16.Dir.Left, new Day16.Dir[] { Day16.Dir.Left })]
+        [InlineData(Day16.Tile.Empty, Day16.Dir.Right, new Day16.Dir[] { Day16.Dir.Right })]
+        [InlineData(Day16.Tile.PrismUpDown, Day16.Dir.Up, new Day16.Dir[] { Day16.Dir.Up})]
+        [InlineData(Day16.Tile.PrismUpDown, Day16.Dir.Down, new Day16.Dir[] { Day16.Dir.Down })]
+        [InlineData(Day16.Tile.PrismUpDown, Day16.Dir.Left, new Day16.Dir[] { Day16.Dir.Up, Day16.Dir.Down })]
+        [InlineData(Day16.Tile.PrismUpDown, Day16.Dir.Right, new Day16.Dir[] { Day16.Dir.Up, Day16.Dir.Down })]
+        [InlineData(Day16.Tile.PrismLeftRight, Day16.Dir.Up, new Day16.Dir[] { Day16.Dir.Left, Day16.Dir.Right })]
+        [InlineData(Day16.Tile.PrismLeftRight, Day16.Dir.Down, new Day16.Dir[] { Day16.Dir.Left, Day16.Dir.Right })]
+        [InlineData(Day16.Tile.PrismLeftRight, Day16.Dir.Left, new Day16.Dir[] { Day16.Dir.Left })]
+        [InlineData(Day16.Tile.PrismLeftRight, Day16.Dir.Right, new Day16.Dir[] { Day16.Dir.Right })]
+        [InlineData(Day16.Tile.MirrorSlash, Day16.Dir.Up, new Day16.Dir[] { Day16.Dir.Right })]
+        [InlineData(Day16.Tile.MirrorSlash, Day16.Dir.Down, new Day16.Dir[] { Day16.Dir.Left })]
+        [InlineData(Day16.Tile.MirrorSlash, Day16.Dir.Left, new Day16.Dir[] { Day16.Dir.Down })]
+        [InlineData(Day16.Tile.MirrorSlash, Day16.Dir.Right, new Day16.Dir[] { Day16.Dir.Up })]
+        [InlineData(Day16.Tile.MirrorBackslash, Day16.Dir.Up, new Day16.Dir[] { Day16.Dir.Left })]
+        [InlineData(Day16.Tile.MirrorBackslash, Day16.Dir.Down, new Day16.Dir[] { Day16.Dir.Right })]
+        [InlineData(Day16.Tile.MirrorBackslash, Day16.Dir.Left, new Day16.Dir[] { Day16.Dir.Up })]
+        [InlineData(Day16.Tile.MirrorBackslash, Day16.Dir.Right, new Day16.Dir[] { Day16.Dir.Down})]
+        public void GetNextDir(Day16.Tile tile, Day16.Dir dir, Day16.Dir[] expected)
+        {
+            // arrange
+            var sut = new Day16();
+
+            // act
+            var actual = sut.GetNextDir(tile, dir).ToArray();
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+
+
+        [Theory]
+        [FileTestData("Day16/sample.in", 46)]
+        [FileTestData("Day16/input.in", 7111)]
+        public void Part1(string input, int expected)
+        {
+            // arrange 
+            var sut = new Day16();
+
+            // act
+            var actual = sut.Part1(input);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [FileTestData("Day16/sample.in", 4)]
+        [FileTestData("Day16/input.in", 41)]
+        public void Part2(string input, int expected)
+        {
+            // arrange 
+            var sut = new Day16();
+
+            // act
+            var actual = sut.Part2(input);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ cs/AdventOfCode.2023/Day16.cs
@@ -0,0 +1,150 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCode.Shared;
+
+namespace AdventOfCode.Y2022
+{
+    [ProblemName("The Floor Will Be Lava")]
+    public class Day16
+    {
+        public int Part1(string input)
+        {
+            var parsed = ParseInput(input);
+            var init = new Beam(0, 0, Dir.Right);
+            var beams = FollowBeam(parsed, init);
+            var result = GetDistinctPositions(beams);
+            return result;
+        }
+
+        public IEnumerable<Beam> FollowBeam(string[] map, Beam init)
+        {
+            var visited = new HashSet<Beam>();
+            var queue = new Queue<Beam>();
+            queue.Enqueue(init);
+
+            while (queue.Any())
+            {
+                var beam = queue.Dequeue();
+                if (visited.Contains(beam)) continue;
+                visited.Add(beam);
+
+                var tile = (Tile) map[beam.row][beam.col];
+                var nextBeams = StepBeam(tile, beam);
+                foreach (var nextBeam in nextBeams)
+                {
+                    if (IsInBounds(map, nextBeam.row, nextBeam.col)) { 
+                        queue.Enqueue(nextBeam);
+                    }
+                }
+            }
+
+            return visited;
+        }
+
+        public bool IsInBounds(string[] map, int row, int col)
+        {
+            return 0 <= row && row < map.Length && 0 <= col && col < map[row].Length;
+        }
+
+        public int GetDistinctPositions(IEnumerable<Beam> beams)
+        {
+            return beams.Select(b => (b.row, b.col)).Distinct().Count();
+        }
+
+        public IEnumerable<Beam> StepBeam(Tile tile, Beam beam)
+        {
+            foreach(var dir in GetNextDir(tile, beam.dir))
+            {
+                var nextPos = GetNextPos(beam.row, beam.col, dir);
+                yield return new Beam(nextPos.row, nextPos.col, dir);
+            }
+        }
+
+        public int Part2(string input)
+        {
+            var parsed = ParseInput(input);
+            throw new NotImplementedException();
+
+        }
+        public string[] ParseInput(string input)
+        {
+            return input.Split("\n");
+        }
+
+        public enum Dir
+        {
+            Up,
+            Down,
+            Left,
+            Right
+        }
+
+        public enum Tile
+        {
+            Empty = '.',
+            PrismUpDown = '|',
+            PrismLeftRight = '-',
+            MirrorSlash = '/',
+            MirrorBackslash = '\\',
+        }
+
+        public (int row, int col) GetNextPos(int row, int col, Dir dir)
+        {
+            return dir switch
+            {
+                Dir.Up => (row - 1, col),
+                Dir.Down => (row + 1, col),
+                Dir.Left => (row, col - 1),
+                Dir.Right => (row, col + 1),
+                _ => throw new ArgumentException("Unknown direction", nameof(dir))
+            };
+        }
+
+        public IEnumerable<Dir> GetNextDir(Tile tile, Dir dir)
+        {
+            switch (tile, dir)
+            {
+                case (Tile.Empty, _):
+                case (Tile.PrismUpDown, Dir.Up):
+                case (Tile.PrismUpDown, Dir.Down):
+                case (Tile.PrismLeftRight, Dir.Left):
+                case (Tile.PrismLeftRight, Dir.Right):
+                    yield return dir;
+                    break;
+                case (Tile.PrismUpDown, Dir.Left):
+                case (Tile.PrismUpDown, Dir.Right):
+                    yield return Dir.Up;
+                    yield return Dir.Down;
+                    break;
+                case (Tile.PrismLeftRight, Dir.Up):
+                case (Tile.PrismLeftRight, Dir.Down):
+                    yield return Dir.Left;
+                    yield return Dir.Right;
+                    break;
+                case (Tile.MirrorSlash, Dir.Right):
+                case (Tile.MirrorBackslash, Dir.Left):
+                    yield return Dir.Up;
+                    break;
+                case (Tile.MirrorSlash, Dir.Down):
+                case (Tile.MirrorBackslash, Dir.Up):
+                    yield return Dir.Left;
+                    break;
+                case (Tile.MirrorSlash, Dir.Left):
+                case (Tile.MirrorBackslash, Dir.Right):
+                    yield return Dir.Down;
+                    break;
+                case (Tile.MirrorSlash, Dir.Up):
+                case (Tile.MirrorBackslash, Dir.Down):
+                    yield return Dir.Right;
+                    break;
+                default:
+                    throw new ArgumentException("Unkown tile/direction", nameof(tile) + ", " + nameof(dir));
+            }
+        }
+
+        public record struct Beam(int row, int col, Dir dir);
+    }
+}
