581271ec3d6920ca641731d840ebff91036c112a
David Lemvigh <dlemvigh@gmail.com>
2025-12-03 13:27:56 +0100

    2025 day 1 part 2

--- vite/src/y2025/day1/day1.test.ts
+++ vite/src/y2025/day1/day1.test.ts
@@ -1,6 +1,6 @@
 import { describe, it, expect } from "vitest";
 import { readInput } from "../../util";
-import { part1, part2 } from "./day1";
+import { countZeroPositionsAndPassesForSingleRotation, countZeroPositionsAndPassesForSingleRotationV2, part1, part2 } from "./day1";
 
 describe("Day 1", () => {
   describe("part 1", () => {
@@ -16,11 +16,54 @@ describe("Day 1", () => {
   describe("part 2", () => {
     it("sample", () => {
       const input = readInput(__dirname, "sample.txt");
-      input && expect(part2(input)).toBe(undefined);
+      input && expect(part2(input)).toBe(6);
     });
     it("input", () => {
       const input = readInput(__dirname, "input.txt");
-      input && expect(part2(input)).toBe(undefined);
+      input && expect(part2(input)).toBe(7101);
     });
   });
+
+  describe("countZeroPositionsAndPassesForSingleRotation", () => {
+    it.each([
+      [50, 0, 0],
+      [50, 1, 0],
+      [50, -1, 0],
+      [50, 50, 1],
+      [50, -50, 1],
+      [50, 100, 1],
+      [50, -100, 1],
+      [50, 150, 2],
+      [50, -150, 2],
+    ])("from %i by %i results in %i", (initPosition, rotation, expected) => {
+      expect(countZeroPositionsAndPassesForSingleRotation(initPosition, rotation)).toBe(expected);
+    })
+
+    describe("part 2 example", () => {
+      it.each([
+        [50, -68, 1],
+        [82, -30, 0],
+      ])("from %i by %i results in %i", (initPosition, rotation, expected) => {
+        expect(countZeroPositionsAndPassesForSingleRotation(initPosition, rotation)).toBe(expected);
+      })
+    })
+  })
+
+  describe("countZeroPositionsAndPassesForSingleRotationV2", () => {
+    it.each([
+      [50, 0, 50, 0],
+      [50, 1, 51, 0],
+      [50, -1, 49, 0],
+      [50, 50, 0, 1],
+      [50, -50, 0, 1],
+      [50, 100, 50, 1],
+      [50, -100, 50, 1],
+      [50, 150, 0, 2],
+      [50, -150, 0, 2],
+
+    ])("from %i by %i results in %i and %i", (initPosition, rotation, expectedPosition, expectedZeroes) => {
+      expect(countZeroPositionsAndPassesForSingleRotationV2(initPosition, rotation)).toEqual({ position: expectedPosition, zeroes: expectedZeroes });
+    })
+  })
+
 });
--- vite/src/y2025/day1/day1.ts
+++ vite/src/y2025/day1/day1.ts
@@ -7,6 +7,9 @@ export function part1(input: string) {
 }
 
 export function part2(input: string)  {
+    const rotations = parseInput(input);
+    const password = countZeroPositionsAndPasses(50, rotations);
+    return password
 
 }
 
@@ -33,4 +36,42 @@ function countZeroPositions(initPosition: number, rotations: number[]): number {
     }
     return positionZeroCount;
 }
-    
+
+function countZeroPositionsAndPasses(initPosition: number, rotations: number[]): number {
+    let position =  initPosition;
+    let positionZeroCount = 0;
+ 
+    for (const rotation of rotations) {
+        const { position: newPosition, zeroes } = countZeroPositionsAndPassesForSingleRotationV2(position, rotation);
+        position = newPosition;
+        positionZeroCount += zeroes;
+    }
+    return positionZeroCount;
+
+}
+
+export function countZeroPositionsAndPassesForSingleRotation(initPosition: number, rotation: number): number {
+    const position = initPosition + rotation;
+    const zeroes = Math.floor(position / 100)
+    if (rotation < 0 && position % 100 === 0) {
+        return Math.abs(zeroes) + 1;
+    } else {
+        return Math.abs(zeroes);
+    }
+}
+
+export function countZeroPositionsAndPassesForSingleRotationV2(initPosition: number, rotation: number): { position: number, zeroes: number } {
+    let position = initPosition + rotation;
+    let zeroes = Math.floor(position / 100)
+
+    position = position + (zeroes * -100)
+
+    if (initPosition === 0 && rotation < 0) {
+        zeroes += 1;
+    }
+    if (position === 0 && rotation < 0) {
+        zeroes -= 1;
+    }
+
+    return { position, zeroes: Math.abs(zeroes) };
+}
